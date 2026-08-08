6b37e44199d392344b43c808780f77f23b364623
L1ZeZzz <3684242047@qq.com>
2026-08-08 11:09:22 +0800

    Update  CloudClientMod.java

--- src/main/java/com/L1Ze/cloudclient/ CloudClientMod.java
+++ src/main/java/com/L1Ze/cloudclient/ CloudClientMod.java
@@ -1,16 +1,16 @@
-package com.yourname.cloudclient;
+package dev.l1zezz.lzclient;
 
-import com.yourname.cloudclient.config.Config;
-import com.yourname.cloudclient.handler.EventHandlers;
-import com.yourname.cloudclient.keybind.KeyBindings;
+import dev.l1zezz.lzclient.config.Config;
+import dev.l1zezz.lzclient.handler.EventHandlers;
+import dev.l1zezz.lzclient.keybind.KeyBindings;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = CloudClientMod.MODID, version = CloudClientMod.VERSION)
+@Mod(modid = CloudClientMod.MODID, name = "LZ Client", version = CloudClientMod.VERSION, clientSideOnly = true)
 public class CloudClientMod {
-    public static final String MODID = "cloudclient";
+    public static final String MODID = "lzclient";
     public static final String VERSION = "1.0.0";
 
     @EventHandler
